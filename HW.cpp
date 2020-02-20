8e86be8f5597034b9839051191de54844936d465
andrea.giuliano@team3d.it <andrea.giuliano@team3d.it>
2020-02-20 20:23:57 +0100

    first commit

---
+++ HW.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    a++;
+    string testString = "lol";
+    cout << a << endl;
+    cout << ++a << endl;
+    cout << "Hello World!" << endl;
+    return 0;
+}
